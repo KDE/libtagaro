e0b5544ee1f5ae2eaa70f6ffc1049398e8f7e23f
Stefan Majewsky <majewsky@gmx.net>
2010-08-13 19:01:05 +0200

    Add KgvTheme, KgvThemeProvider and KgvDesktopThemeProvider classes.

---
+++ visuals/theming/kgvtheme.cpp
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *   Copyright 2010 Stefan Majewsky <majewsky@gmx.net>                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License          *
+ *   version 2 as published by the Free Software Foundation                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include "kgvtheme.h"
+
+struct KgvTheme::Private
+{
+	QByteArray m_identifier;
+	QHash<KgvTheme::Role, QVariant> m_values;
+	QHash<QByteArray, QVariant> m_customValues;
+
+	Private(const QByteArray& identifier) : m_identifier(identifier) {}
+};
+
+KgvTheme::KgvTheme(const QByteArray& identifier)
+	: d(new Private(identifier))
+{
+}
+
+KgvTheme::~KgvTheme()
+{
+	delete d;
+}
+
+QVariant KgvTheme::data(KgvTheme::Role role, const QVariant& defaultValue) const
+{
+	return d->m_values.value(role, defaultValue);
+}
+
+QVariant KgvTheme::data(const QByteArray& key, const QVariant& defaultValue) const
+{
+	return d->m_customValues.value(key, defaultValue);
+}
+
+void KgvTheme::setData(KgvTheme::Role role, const QVariant& value)
+{
+	d->m_values.insert(role, value);
+}
+
+void KgvTheme::setData(const QByteArray& key, const QVariant& value)
+{
+	d->m_customValues.insert(key, value);
+}
+
+QByteArray KgvTheme::identifier() const
+{
+	return d->m_identifier;
+}
---
+++ visuals/theming/kgvtheme.h
@@ -0,0 +1,86 @@
+/***************************************************************************
+ *   Copyright 2010 Stefan Majewsky <majewsky@gmx.net>                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License          *
+ *   version 2 as published by the Free Software Foundation                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef LIBKGAMEVISUALS_THEME_DECLARE_H
+#define LIBKGAMEVISUALS_THEME_DECLARE_H
+
+#include <QtCore/QVariant>
+
+#include <libkgame_export.h>
+
+/**
+ * @class KgvTheme kgvtheme.h <KgvTheme>
+ *
+ * A theme is an entity which essentially represents an SVG file plus some
+ * metadata. KgvTheme instances are usually created and managed by a 
+ * KgvThemeProvider.
+ */
+class KGAMEVISUALS_EXPORT KgvTheme
+{
+	Q_DISABLE_COPY(KgvTheme)
+	public:
+		///Each theme has a set of data elements associated with it, each with
+		///its own role. The data in given roles is used by user interfaces to
+		///display information about the themes, or to determine the files
+		///associated with this theme.
+		///@see setData(), data()
+		enum Role
+		{
+			///This role *must* be filled with the path of the SVG file which is
+			///represented by this theme. If this role is empty or contains
+			///invalid data, loading the theme into a KgvRenderer will fail.
+			GraphicsFileRole,
+			///If the data in this KgvTheme instance was read from a file, this
+			///role may contain the path to that file.
+			ThemeFileRole,
+			///The name of this theme.
+			NameRole,
+			///An additional description which goes beyond the Name.
+			DescriptionRole,
+			///(optional) The name of this theme's author.
+			AuthorRole,
+			///(optional) The email address of this theme's author.
+			AuthorEmailRole,
+			///(optional) A preview image showing the visual appearance of the
+			///theme.
+			PreviewRole
+		};
+
+		///Creates a new KgvTheme with the given @a identifier. The identifier
+		///must be unique among themes created by one KgvThemeProvider.
+		KgvTheme(const QByteArray& identifier);
+		///Destroys this KgvTheme instance.
+		~KgvTheme();
+
+		///@return the data stored unter the given @a role, or the given
+		///@a defaultValue if no data is stored
+		QVariant data(KgvTheme::Role role, const QVariant& defaultValue = QVariant()) const;
+		///@overload for custom data
+		QVariant data(const QByteArray& key, const QVariant& defaultValue = QVariant()) const;
+		///Stores the given @a value unter the given @a role.
+		void setData(KgvTheme::Role role, const QVariant& value);
+		///@overload for custom data
+		void setData(const QByteArray& key, const QVariant& value);
+		///@return the internal identifier for this theme
+		QByteArray identifier() const;
+	private:
+		class Private;
+		Private* const d;
+};
+
+#endif // LIBKGAMEVISUALS_THEME_DECLARE_H
