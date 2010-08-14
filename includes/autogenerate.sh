77fb3e1bd5379dc79ead5133cec949f31eaf845c
Stefan Majewsky <majewsky@gmx.net>
2010-08-14 12:48:52 +0200

    Install <KgvDesktopThemeProvider> include.

--- includes/autogenerate.sh
+++ includes/autogenerate.sh
@@ -2,11 +2,17 @@
 # This script finds all headers (except for private headers) in the known
 # library source trees, looks for exported classes inside there, and generates
 # forward includes for all these classes.
-# NOTE: You have to run this from the directory where this script is located!
+# Calling syntax: sh autogenerate.sh ../visuals > CMakeLists.txt
 
-find ../visuals -name \*.h -a \! -name \*_p.h | while read HEADERFILE; do
-	# FIXME: This does not recognize namespaces (that doesn't hurt because we do not use any as of now)
-	grep 'class KGAMEVISUALS_EXPORT' $HEADERFILE | sed 's/^.*EXPORT \([^ ]*\).*$/\1/' | while read CLASSNAME; do
+echo 'install(FILES'
+while [ -d "$1" ]; do
+	find "$1" -name \*.h -a \! -name \*_p.h | while read HEADERFILE; do
+		# FIXME: This does not recognize namespaces (that doesn't hurt because we do not use any as of now)
+		grep 'class KGAMEVISUALS_EXPORT' $HEADERFILE | sed 's/^.*EXPORT \([^ ]*\).*$/\1/' | while read CLASSNAME; do
 			echo '#include <'$(basename $HEADERFILE)'>' > $CLASSNAME
-	done
+			echo -en "\t"; echo "$CLASSNAME"
+		done
+	done | sort
+	shift
 done
+echo 'DESTINATION ${INCLUDE_INSTALL_DIR}/KDE COMPONENT Devel)'
